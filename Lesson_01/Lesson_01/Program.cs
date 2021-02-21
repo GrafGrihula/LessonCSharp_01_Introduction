32c893a775c19f3580cbb4d62b4a718a7d073a75
GrafGrihula <48409840+GrafGrihula@users.noreply.github.com>
2021-02-21 23:22:31 +0300

    Проекты решений задач по Уроку № 2 (Lesson_02). Все "решения" задач по Уроку пишу в одном проекте. Так Вам удобно проверять или каждое решение делать отдельным проектом/решением?

---
+++ Lesson_01/Lesson_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_01
+{
+    class Program
+    {
+        /// <summary>
+        /// Ввод имени и вывод имени и текущей даты (без времени)
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}, сегодня {DateTime.Today.ToShortDateString()} года");
+            Console.Read();
+        }
+    }
+}
