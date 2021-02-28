33228ea05c876dcf2c802161527d33a580bea288
GrafGrihula <48409840+GrafGrihula@users.noreply.github.com>
2021-02-28 22:16:06 +0300

    ДЗ по уроку № 4

---
+++ Lesson_04/FibonacciNumber/ProgramFibonacciNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FibonacciNumber
+{
+    class ProgramFibonacciNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение для вычисления числа Фибоначчи: ");
+            int index = 0;
+            index = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Число Фибоначчи для значения n={index} равно {FibonacciCalc(index)}");
+            Console.Read();
+        }
+
+        static int FibonacciCalc(int index)
+        {
+            return FibonacciCalc(index, out _);
+        }
+
+        static int FibonacciCalc(int index, out int F1)
+        {
+            F1 = 0;
+            if (index == 0)
+            {
+                return 0;
+            }
+            else if (index == 1)
+            {
+                return 1;
+            }
+            else 
+            {
+                int F2;
+                F1 = FibonacciCalc(index - 1, out F2);
+                return F1 + F2;
+            }
+        }
+    }
+}
