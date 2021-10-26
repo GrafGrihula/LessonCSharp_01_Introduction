be861a77c11612fb8f83c41f0168214ff22211c4
GrafGrihula <grafgrihula@yandex.ru>
2021-10-26 21:37:41 +0300

    ДЗ № 8

---
+++ Lesson_08/SetSaveForm/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetSaveForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.option1 = checkBox1.Checked;
+            Properties.Settings.Default.option2 = checkBox2.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Reload();
+            checkBox1.Checked = Properties.Settings.Default.option1;
+            checkBox2.Checked = Properties.Settings.Default.option2;
+        }
+    }
+}
