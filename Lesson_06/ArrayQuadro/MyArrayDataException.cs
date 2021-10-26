5810f66abbab922d1e62be6a20daea746a1d03ca
GrafGrihula <grafgrihula@yandex.ru>
2021-10-26 21:35:46 +0300

    ДЗ № 6

---
+++ Lesson_06/ArrayQuadro/MyArrayDataException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayQuadro
+{
+    [Serializable]
+    class MyArrayDataException : Exception
+    {
+        int row, column;
+
+        public MyArrayDataException(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+    }
+}
---
+++ Lesson_06/Staffer/ProgramStaffer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Staffer
+{
+    class ProgramStaffer
+    {
+        static void Main(string[] args)
+        {
+            //1.	Создать класс "Сотрудник" с полями: ФИО, должность, телефон,
+            //зарплата, возраст;
+            RandomText rt = new RandomText();
+
+            Employee e = new Employee(rt.RandomFamiliya(), rt.RandomName(),
+                    rt.RandomOtchestvo(), rt.RandomTelephone(), rt.RandomEmail(),
+                    rt.RandomPost(), int.Parse( rt.RandomSalary()), int.Parse(rt.RandomAge()));
+
+             //   5.Создать массив из 5 сотрудников.С помощью цикла вывести
+             //информацию только о сотрудниках старше 40 лет;
+
+            Employee[] employees = {
+            e,
+            new Employee(rt.RandomFamiliya(), rt.RandomName(),
+                    rt.RandomOtchestvo(), rt.RandomTelephone(), rt.RandomEmail(),
+                    rt.RandomPost(), int.Parse( rt.RandomSalary()), int.Parse(rt.RandomAge())),
+            new Employee(rt.RandomFamiliya(), rt.RandomName(),
+                    rt.RandomOtchestvo(), rt.RandomTelephone(), rt.RandomEmail(),
+                    rt.RandomPost(), int.Parse( rt.RandomSalary()), int.Parse(rt.RandomAge())),
+            new Employee(rt.RandomFamiliya(), rt.RandomName(),
+                    rt.RandomOtchestvo(), rt.RandomTelephone(), rt.RandomEmail(),
+                    rt.RandomPost(), int.Parse( rt.RandomSalary()), int.Parse(rt.RandomAge())),
+            new Employee(rt.RandomFamiliya(), rt.RandomName(),
+                    rt.RandomOtchestvo(), rt.RandomTelephone(), rt.RandomEmail(),
+                    rt.RandomPost(), int.Parse( rt.RandomSalary()), int.Parse(rt.RandomAge()))
+            };
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].getAge() > 40)
+                    Console.WriteLine(employees[i].GetFullInfo());
+            }
+
+            Console.WriteLine("--------------------------------------------------------");
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(employees[i].GetFullInfo());
+            }
+
+            Console.WriteLine("--------------------------------------------------------");
+        }
+    
+
+        
+    }
+}
